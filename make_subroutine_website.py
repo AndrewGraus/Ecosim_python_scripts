c1690949f7f1c75686c0b1fd4220cb56fa75b6c2
AndrewGraus <agraus@lbl.gov>
2022-01-21 15:10:39 -0800

    modifications to make the subroutine html files

--- make_subroutine_website.py
+++ make_subroutine_website.py
@@ -10,7 +10,7 @@
 
 import numpy as np
 import sys, subprocess, os, re
-import argparse
+import argparse, pathlib
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--dir', dest="ecomoddir", type=str, nargs=1, default=[""],
@@ -23,7 +23,10 @@ print(ecodir)
 ecofile_list = os.listdir(ecodir)
 ecofile_list = [xx for xx in ecofile_list if xx.endswith('.f')]
 
-htmlnew=open("./ecosim_subroutines.html","w")
+#Make the directory for the files to go in
+pathlib.Path("./ecosim_website/subroutine_htmls/").mkdir(parents=True,exist_ok=True)
+
+htmlnew=open("./ecosim_website/ecosim_subroutines.html","w")
 
 #read in the js functions, ccs styles, and beginning of the table
 #then write to the new file
@@ -32,12 +35,9 @@ with open("./preamble.html","r") as prehtml:
     for line in lines:
         htmlnew.write(line)
         
-#start the html file
-#htmlnew.write('<!DOCTYPE html>\n<html>\n<body>')
-
 #write the top level dir for the modules
 htmlnew.write('<tr class="parent">\n<td class="row-label">'+str(ecodir)+'</td>\n')
-website_link = 'test.html'
+#website_link = 'test.html'
 
 #loop where we read over every module and print it subroutines
 for ecosim_file in ecofile_list:
@@ -46,14 +46,27 @@ for ecosim_file in ecofile_list:
         for line in lines:
             if line.strip().startswith('module'):
                 line_split = re.split(' ',line.strip())
-                #htmlnew.write('<h>'+line.strip()+'</h>')
                 htmlnew.write('<tr class="child_variable">\n<td class="row-label">&nbsp;&nbsp;&nbsp;&nbsp;'+line_split[1]+'</td>')
             elif line.lower().strip().startswith('subroutine'):
                 htmlnew.write('\n')
-                #htmlnew.write('<p>'+line.strip()+'</p>\n\n')
                 line_split = re.split(' |[(|)]',line.strip())
-                print(line_split)
-                htmlnew.write('<tr class="child_dataset">\n<td class="row-label"><a href="'+website_link+'" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+line_split[1]+'</a></td>')
+                #genrate subroutine html file
+
+                #print(line_split)
+                website_link = './subroutine_htmls/'+line_split[1]+'.html'
+                f_sub_html = open('./ecosim_website/'+website_link,'w')
+                f_sub_html.write('<html>\n\t<body>')
+                f_sub_html.write('\t\t<h2>'+line_split[1]+'</h2>')
+                if len(line_split)>2:
+                    f_sub_html.write('\t\t<p>Input Variables: '+line_split[2]+'</p>')
+                else:
+                    f_sub_html.write('\t\t<p>Input Variables: None</p>')
+                #end subroutine file
+                f_sub_html.write('\t</body>\n</html>')
+                #print(line_split)
+                htmlnew.write('<tr class="child_dataset">\n<td class="row-label"><a href="'+website_link+
+                              '" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+                              line_split[1]+'</a></td>')
                 #new_subroutine = True
             #elif line.lower().strip().startswith('call'):
             #    if new_subroutine == True:
